e272419b43771f1fcecb7552b524c2c41b393ffb
钟宁 <zhongning@cjis.wan>
2023-07-06 10:23:25 +0800

    上传代码

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import { sum } from 'lodash-es'
 
 console.log('sum', sum([1, 2, 3, 4]))
+
+// dep
+{
+  subs: [Watcher]
+}
+
+// Watcher
+{
+  dep: [dep]
+}
+
+this.a = 1
+this.b = 2
+nextTick()
+
+a: {
+  subs: [computedWatcher, Watcher]
+}
